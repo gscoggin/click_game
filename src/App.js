94cc0f800854b447e8c7f183bef4b76341e68574
gscoggin <gabescoggin@gmail.com>
2018-10-05 08:07:49 -0700

    updating the game to change the on click events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
 class App extends Component {
+  
+  state = {
+    friends
+  };
+  
+  shuffleFriend = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+
+  handleClick = id => {
+    console.log("The image was clicked");
+    
+    this.setState({ friends: this.shuffleFriend(this.state.friends) })
+
+    }
+
+  removeFriend = id => {
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+  
+    this.setState({ friends });
+
+  };
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Wrapper>
+          <Title>Friends List</Title>
+          {this.state.friends.map(friend => (
+            <FriendCard
+              handleClick={this.handleClick}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+              occupation={friend.occupation}
+              location={friend.location}
+            />
+            ))}
+        </Wrapper>
       </div>
     );
   }
